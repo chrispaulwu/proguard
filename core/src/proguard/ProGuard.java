de1ccb30257c864d6224f3db1f7e295bba1bad85
tomystang <tomystang@tencent.com>
2021-01-20 22:17:48 +0800

    Update version to 6.2.2-wc

--- core/src/proguard/ProGuard.java
+++ core/src/proguard/ProGuard.java
@@ -44,7 +44,7 @@ import java.io.*;
  */
 public class ProGuard
 {
-    public static final String VERSION = "ProGuard, version 6.2.2";
+    public static final String VERSION = "ProGuard, version 6.2.2-wc";
 
     private final Configuration configuration;
     private       ClassPool     programClassPool = new ClassPool();
